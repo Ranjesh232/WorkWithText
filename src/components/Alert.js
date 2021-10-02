907cea11d8124d0fbb6b93e9c2d6f3dc1045fbc7
Ranjesh Prabhat <rsprabhat2000@gmail.com>
2021-10-02 19:58:56 +0530

    Removed some bugs

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,8 +8,10 @@ function Alert(props) {
     }
 
     return (
-        props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-            <strong>{capitaized(props.alert.type)}</strong>: <strong>{props.alert.msg}</strong>
+        <div style={{ height: "50px" }}>
+            {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                <strong>{capitaized(props.alert.type)}</strong>: <strong>{props.alert.msg}</strong>
+            </div>}
         </div>
     )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -37,6 +37,7 @@ export default function TextForm(props) {
         let text = document.getElementById("textBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Text Copied.", "success")
     }
     const handleClearText = () => {
@@ -53,24 +54,24 @@ export default function TextForm(props) {
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <div className="mb-3">
                     <h2>{props.heading}</h2>
-                    <textarea className="form-control" style={{ backgroundColor: props.mode === 'dark' ? 'white' : 'white', color: props.mode === 'dark' ? 'black' : 'black' }} onChange={handleOnChange} value={text} placeholder="Enter your text here..." id="textBox" rows="6"></textarea>
+                    <textarea className="form-control" style={{ backgroundColor: props.mode === 'dark' ? '#13466e' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} onChange={handleOnChange} value={text} placeholder="Enter your text here..." id="textBox" rows="6"></textarea>
                 </div>
-                <button className="btn btn-primary m-1 p-1 mx-1" onClick={handleUpperCase} >Convert to UpperCase</button>
-                <button className="btn btn-primary m-1 p-1 mx-1" onClick={handleLowerCase} >Convert to LowerCase</button>
-                <button className="btn btn-primary m-1 p-1 mx-1" onClick={handleTitleCase} >Convert to TitleCase</button>
-                <button className="btn btn-primary m-1 p-1 mx-1" onClick={handleRevCase} >Reverse the Text</button>
-                <button className="btn btn-primary m-1 p-1 mx-1" onClick={handleCopy} >Copy Text</button>
-                <button className="btn btn-primary m-1 p-1 mx-1" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
-                <button className="btn btn-primary m-1 p-1 mx-1" onClick={handleClearText} >Clear Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary m-1 p-1 mx-1" onClick={handleUpperCase} >Convert to UpperCase</button>
+                <button disabled={text.length === 0} className="btn btn-primary m-1 p-1 mx-1" onClick={handleLowerCase} >Convert to LowerCase</button>
+                <button disabled={text.length === 0} className="btn btn-primary m-1 p-1 mx-1" onClick={handleTitleCase} >Convert to TitleCase</button>
+                <button disabled={text.length === 0} className="btn btn-primary m-1 p-1 mx-1" onClick={handleRevCase} >Reverse the Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary m-1 p-1 mx-1" onClick={handleCopy} >Copy Text</button>
+                <button disabled={text.length === 0} className="btn btn-primary m-1 p-1 mx-1" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
+                <button disabled={text.length === 0} className="btn btn-primary m-1 p-1 mx-1" onClick={handleClearText} >Clear Text</button>
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your Text Summary:</h2>
-                <p>{text.length > 0 ? text.trim().split(" ").length : 0} words and {text.length} characters.</p>
-                <p>Time required to read the text: {0.008 * text.split(" ").length}</p>
+                <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters.</p>
+                <p>Time required to read the text: {0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length}</p>
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Preview of your Text:</h2>
-                <p>{text}</p>
+                <p>{text.length > 0 ? text : "Enter Something to Preview!"}</p>
             </div>
         </>
     )
